30a3cb85b10f6345e8a9322f216b86147d7b2b37
dirkarnez <smalldirkalex@gmail.com>
2020-11-30 19:25:06 +0800

    - add lookup_table.dll

---
+++ Assets/CppButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class CppButton : MonoBehaviour
+{
+    [DllImport("lookup_table")]
+    private extern static void GetRandomNumberArrayOfLength10(int[] arr);
+
+    public void TestGetRandomNumberArrayOfLength10()
+    {
+        int[] array = new int[10];
+
+        GetRandomNumberArrayOfLength10(array);
+
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+}
